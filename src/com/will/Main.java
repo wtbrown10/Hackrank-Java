247f15d8695bb6f86945838fb2ba2fe0052ca94c
Will brown <wtbrown10@gmail.com>
2021-04-05 20:37:01 -0400

    day1

--- src/com/will/Main.java
+++ src/com/will/Main.java
@@ -1,5 +1,7 @@
 package com.will;
 
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -7,5 +9,6 @@ public class Main {
         // write your code here
         Day2 day2 = new Day2();
         day2.solve(12.00, 20, 8);
+        Scanner scan = new Scanner()
     }
 }
